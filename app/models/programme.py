0e07fc1e6d5d42a445a9483a9318d4a4d502de99
Michael Akrobo <88484498+360konet@users.noreply.github.com>
2023-08-30 23:15:30 +0000

    Update programme.py

--- app/models/programme.py
+++ app/models/programme.py
@@ -1,6 +1,14 @@
 import uuid
 from sqlmodel import Column, Field, ForeignKey, Enum, relationship, SQLModel
 
+class Student(SQLModel, table=True):
+    __tablename__ = 'students'
+    
+    id: int = Field(primary_key=True)
+    name: str = Field(index=True)
+    
+    programs: list["Program"] = relationship("Program", back_populates="student")
+
 class Program(SQLModel, table=True):
     __tablename__ = 'programs'
     
@@ -11,4 +19,4 @@ class Program(SQLModel, table=True):
     entry_level: str = Field(Enum('Level 1', 'Level 2', 'Level 3', name='entry_level_enum'), nullable=False)
     hall: str = Field(Enum('Hall A', 'Hall B', 'Hall C', name='hall_enum'), nullable=False)
     
-    student: "Student" = relationship("Student", back_populates="programs")
+    student: Student = relationship("Student", back_populates="programs")
