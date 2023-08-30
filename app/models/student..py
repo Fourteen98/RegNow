2a9d09775799e21c137b02a2a4b280bc013147c9
Napaa Samuel <128978678+Thenebit@users.noreply.github.com>
2023-08-30 17:03:31 +0000

    Update student..py

--- app/models/student..py
+++ app/models/student..py
@@ -1,10 +1,10 @@
-from sqlmodel import SQLModel, Field
-
+from sqlmodel import SQLModel, Field, Column
+import uuid
 
 
 class Student(SQLModel, table=True):
-    __tablename__ = 'student'
-    id: int = Field(default=None, primary_key=True)
+    __tablename__ = 'students'
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     first_name: str
     last_name: str
     middle_name: str
