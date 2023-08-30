b17e500571eab7fe26d60943d4d0511a98fa274a
Michael Akrobo <88484498+360konet@users.noreply.github.com>
2023-08-30 23:07:30 +0000

    Update programme.py

--- app/models/programme.py
+++ app/models/programme.py
@@ -3,8 +3,8 @@ import uuid
 from sqlmodel import Column, Field, SQLModel
 
 
-class Program(SQLModel, table=True):
-    __tablename__ = 'programs'
-      id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+class Bird(SQLModel, table=True):
+    __tablename__ = 'birds'
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     name: str = Field(index=True)
     color: str
