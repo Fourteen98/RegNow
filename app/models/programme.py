c85e80c443beb86176a79bd608a36188d08ff7c8
Michael Akrobo <88484498+360konet@users.noreply.github.com>
2023-08-30 23:00:56 +0000

    Update programme.py

--- app/models/programme.py
+++ app/models/programme.py
@@ -4,7 +4,12 @@ from sqlmodel import Column, Field, SQLModel
 
 
 class Program(SQLModel, table=True):
-    __tablename__ = 'birds'
+    __tablename__ = 'programs'
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    name: str = Field(index=True)
-    color: str
+    n student_id = Field(Integer, ForeignKey('students.id'), nullable=False)
+    programme_name = Field(Enum('Program A', 'Program B', name='programme_name_enum'), nullable=False)
+    stream = Field(Enum('Stream X', 'Stream Y', 'Stream Z', name='stream_enum'), nullable=False)
+    entry_level = Field(Enum('Level 1', 'Level 2', 'Level 3', name='entry_level_enum'), nullable=False)
+    hall = Field(Enum('Hall A', 'Hall B', 'Hall C', name='hall_enum'), nullable=False)
+    
+    student = relationship("Student", back_populates="programs")
