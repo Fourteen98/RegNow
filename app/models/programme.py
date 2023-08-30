56c31fa71aac4a0ad16b5b3277179f7d9eef552e
Michael Akrobo <88484498+360konet@users.noreply.github.com>
2023-08-30 22:44:39 +0000

    Update programme.py

--- app/models/programme.py
+++ app/models/programme.py
@@ -15,20 +15,20 @@ Base = declarative_base()
 class Program(Base):
     __tablename__ = 'programs'
     
-    id = Field(Integer, primary_key=True)
-    student_id = Field(Integer, ForeignKey('students.id'), nullable=False)
+    id = Column(Integer, primary_key=True)
+    student_id = Column(Integer, ForeignKey('students.id'), nullable=False)
     programme_name = Column(Enum('Program A', 'Program B', name='programme_name_enum'), nullable=False)
-    stream = Field(Enum('Stream X', 'Stream Y', 'Stream Z', name='stream_enum'), nullable=False)
-    entry_level =Field(Enum('Level 1', 'Level 2', 'Level 3', name='entry_level_enum'), nullable=False)
-    hall = Field(Enum('Hall A', 'Hall B', 'Hall C', name='hall_enum'), nullable=False)
+    stream = Column(Enum('Stream X', 'Stream Y', 'Stream Z', name='stream_enum'), nullable=False)
+    entry_level = Column(Enum('Level 1', 'Level 2', 'Level 3', name='entry_level_enum'), nullable=False)
+    hall = Column(Enum('Hall A', 'Hall B', 'Hall C', name='hall_enum'), nullable=False)
     
     student = relationship("Student", back_populates="programs")
 
 class Student(Base):
     __tablename__ = 'students'
     
-    id = Field(Integer, primary_key=True)
-    name = Field(String, nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
     
     programs = relationship("Program", back_populates="student")
 
@@ -37,3 +37,26 @@ class Student(Base):
         if not name:
             raise ValueError("Name cannot be empty.")
         return name
+
+# Create tables in the database
+Base.metadata.create_all(engine)
+
+# Create a session to interact with the database
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Example usage: Adding a student and program
+new_student = Student(name="John Doe")
+session.add(new_student)
+session.commit()
+
+new_program = Program(student=new_student, programme_name='Program A', stream='Stream X', entry_level='Level 1', hall='Hall A')
+session.add(new_program)
+session.commit()
+
+# Generate Alembic migration
+alembic_cfg = Config("alembic.ini")  # Point this to your Alembic configuration file
+alembic_cfg.set_main_option("script_location", "your_script_location")
+command.revision(alembic_cfg, autogenerate=True, message="Create students and programs tables")
+
+session.close()
