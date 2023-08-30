c016bc8b7df2d7413e41425ae480a9ab0ee2d535
Michael Akrobo <88484498+360konet@users.noreply.github.com>
2023-08-30 23:46:56 +0000

    Update programme.py

--- app/models/programme.py
+++ app/models/programme.py
@@ -11,12 +11,10 @@ class Student ( SQLModel, table=True):
 
 class Program(SQLModel, table=True):
     __tablename__ = 'programs'
-    
     id: uuid.UUID = Field(default_factory=uuid.uuid4,primary_key=True)
     student_id: int = Field(ForeignKey('students.id'),nullable=False)
     programme_name: str = Field(Enum('Program A', 'Program B', name='programme_name_enum'), nullable=False)
     stream: str = Field(Enum('Stream X', 'Stream Y', 'Stream Z', name='stream_enum'), nullable=False)
     entry_level: str = Field(Enum('Level 1', 'Level 2', 'Level 3', name='entry_level_enum'), nullable=False)
     hall: str = Field(Enum('Hall A', 'Hall B', 'Hall C', name='hall_enum'), nullable=False)
-    
     student: Student = relationship("Student", back_populates="programs")
