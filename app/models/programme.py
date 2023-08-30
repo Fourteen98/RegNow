0636d3ec2227e2f2eb82beb50702ea069785fcc2
Michael Akrobo <88484498+360konet@users.noreply.github.com>
2023-08-30 23:12:42 +0000

    Update programme.py

--- app/models/programme.py
+++ app/models/programme.py
@@ -1,10 +1,14 @@
 import uuid
+from sqlmodel import Column, Field, ForeignKey, Enum, relationship, SQLModel
 
-from sqlmodel import Column, Field, SQLModel
-
-
-class Programme(SQLModel, table=True):
-    __tablename__ = 'programmes'
+class Program(SQLModel, table=True):
+    __tablename__ = 'programs'
+    
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    name: str = Field(index=True)
-    color: str
+    student_id: int = Field(ForeignKey('students.id'), nullable=False)
+    programme_name: str = Field(Enum('Program A', 'Program B', name='programme_name_enum'), nullable=False)
+    stream: str = Field(Enum('Stream X', 'Stream Y', 'Stream Z', name='stream_enum'), nullable=False)
+    entry_level: str = Field(Enum('Level 1', 'Level 2', 'Level 3', name='entry_level_enum'), nullable=False)
+    hall: str = Field(Enum('Hall A', 'Hall B', 'Hall C', name='hall_enum'), nullable=False)
+    
+    student: "Student" = relationship("Student", back_populates="programs")
