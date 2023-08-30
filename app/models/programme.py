fd70b150d9bed7a277a280a1c2aa84a75c6dbe75
Michael Akrobo <88484498+360konet@users.noreply.github.com>
2023-08-30 22:19:06 +0000

    Update programme.py

--- app/models/programme.py
+++ app/models/programme.py
@@ -15,7 +15,7 @@ class Program(SQLModel, table=True):
     
     id = Field(Integer, primary_key=True)
     student_id = Field(Integer, ForeignKey('students.id'), nullable=False)
-    programme_name = Field(Enum('Computer Science', 'Computer Networking','Marketing','Financal Accounting', name='programme_name_enum'), nullable=False)
+    programme_name = Field(Enum('Computer Science', 'Computer Networking', 'Marketing', 'Financal Accounting', name='programme_name_enum'), nullable=False)
     stream = Field(Enum('Stream X', 'Stream Y', 'Stream Z', name='stream_enum'), nullable=False)
     entry_level = Field(Enum('Level 1', 'Level 2', 'Level 3', name='entry_level_enum'), nullable=False)
     hall = Field(Enum('Hall A', 'Hall B', 'Hall C', name='hall_enum'), nullable=False)
