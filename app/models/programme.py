e3d9763eea222ebe7b3fb7ae93ada11ae336836b
Michael Akrobo <88484498+360konet@users.noreply.github.com>
2023-08-30 23:08:00 +0000

    Update programme.py

--- app/models/programme.py
+++ app/models/programme.py
@@ -4,7 +4,7 @@ from sqlmodel import Column, Field, SQLModel
 
 
 class Bird(SQLModel, table=True):
-    __tablename__ = 'birds'
+    __tablename__ = 'programmes'
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     name: str = Field(index=True)
     color: str
