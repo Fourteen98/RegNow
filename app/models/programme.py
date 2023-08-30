7d9e4d01de33d434ade9b3c6b607cbbebb544f12
Michael Akrobo <88484498+360konet@users.noreply.github.com>
2023-08-30 22:53:55 +0000

    Update programme.py

--- app/models/programme.py
+++ app/models/programme.py
@@ -1,5 +1,10 @@
 import uuid
-
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Enum
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import validates
+from alembic import command
+from alembic.config import Config
 from sqlmodel import Column, Field, SQLModel
 
 
