c3a392253b709544683f2efc9462efcd0e9614ca
Michael Akrobo <88484498+360konet@users.noreply.github.com>
2023-08-30 21:26:41 +0000

    Update programme.py

--- app/models/programme.py
+++ app/models/programme.py
@@ -13,20 +13,20 @@ from sqlmodel import Column, Field, SQLModel
 class Program(SQLModel, table=True):
     __tablename__ = 'programs'
     
-    id = Column(Integer, primary_key=True)
-    student_id = Column(Integer, ForeignKey('students.id'), nullable=False)
-    programme_name = Column(Enum('Program A', 'Program B', name='programme_name_enum'), nullable=False)
-    stream = Column(Enum('Stream X', 'Stream Y', 'Stream Z', name='stream_enum'), nullable=False)
-    entry_level = Column(Enum('Level 1', 'Level 2', 'Level 3', name='entry_level_enum'), nullable=False)
-    hall = Column(Enum('Hall A', 'Hall B', 'Hall C', name='hall_enum'), nullable=False)
+    id = Field(Integer, primary_key=True)
+    student_id = Field(Integer, ForeignKey('students.id'), nullable=False)
+    programme_name = Field(Enum('Computer Science', 'Computer Networking','Marketing','Financal Accounting', name='programme_name_enum'), nullable=False)
+    stream = Field(Enum('Stream X', 'Stream Y', 'Stream Z', name='stream_enum'), nullable=False)
+    entry_level = Field(Enum('Level 1', 'Level 2', 'Level 3', name='entry_level_enum'), nullable=False)
+    hall = Field(Enum('Hall A', 'Hall B', 'Hall C', name='hall_enum'), nullable=False)
     
     student = relationship("Student", back_populates="programs")
 
-class Student(Base):
+class Student(SQLModel, table=True):
     __tablename__ = 'students'
     
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    id = Field(Integer, primary_key=True)
+    name = Field(String, nullable=False)
     
     programs = relationship("Program", back_populates="student")
 
