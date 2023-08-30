97dc54e7593d3905598745c16c3143e58c475238
Michael Akrobo <88484498+360konet@users.noreply.github.com>
2023-08-30 23:02:47 +0000

    Update programme.py

--- app/models/programme.py
+++ app/models/programme.py
@@ -6,7 +6,7 @@ from sqlmodel import Column, Field, SQLModel
 class Program(SQLModel, table=True):
     __tablename__ = 'programs'
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    n student_id = Field(Integer, ForeignKey('students.id'), nullable=False)
+    student_id = Field(Integer, ForeignKey('students.id'), nullable=False)
     programme_name = Field(Enum('Program A', 'Program B', name='programme_name_enum'), nullable=False)
     stream = Field(Enum('Stream X', 'Stream Y', 'Stream Z', name='stream_enum'), nullable=False)
     entry_level = Field(Enum('Level 1', 'Level 2', 'Level 3', name='entry_level_enum'), nullable=False)
